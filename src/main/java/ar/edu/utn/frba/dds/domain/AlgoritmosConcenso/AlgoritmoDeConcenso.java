0689f704060f9dd236c8539005dcab7fda76a448
nico-DSK <schkurkonicolas@gmail.com>
2025-07-03 23:41:40 -0300

    Fix: rename AlgoritmoDeConcenso to AlgoritmoDeConsenso and related issues

--- src/main/java/ar/edu/utn/frba/dds/domain/AlgoritmosConcenso/AlgoritmoDeConcenso.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.utn.frba.dds.domain.algoritmosconcenso;
-
-import ar.edu.utn.frba.dds.domain.fuentes.Fuente;
-import ar.edu.utn.frba.dds.domain.hecho.Hecho;
-import java.util.List;
-
-public interface AlgoritmoDeConcenso {
-  public List<Hecho> listaDeHechosConcensuados(List<Hecho> listaDeHechos, List<Fuente> fuentesNodo);
-}
